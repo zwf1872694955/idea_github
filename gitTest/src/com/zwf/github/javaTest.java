0d894661be9d38be85916274d1b36c038c686aa2
zwf <1872694955@qq.com>
2020-03-07 17:15:03 +0800

    hello git   my commit frist git!

---
+++ gitTest/src/com/zwf/github/javaTest.java
@@ -0,0 +1,12 @@
+package com.zwf.github;
+
+public class javaTest {
+
+    public static void main(String[] args) {
+        System.out.println("hello git！");
+
+        int i=0;
+        i=i+1;
+        System.out.println(i);
+    }
+}
